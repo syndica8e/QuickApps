398f693a449318909a4587fae29214877e7bfa20
syndica8e <127806332+syndica8e@users.noreply.github.com>
2024-04-14 23:34:29 -0700

    Display Cards, Sum
    function for start button
    Display Cards
    Display Sum

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -4,17 +4,23 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
-
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function startGame() {
+    // 3. Render the cars on the page using this format -> "Cards: 10 4"
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-console.log(message)
